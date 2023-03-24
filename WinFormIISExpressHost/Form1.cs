7f116c1a5fa0c99a843a0e48371704fc9de5ad21
kiba518 <liujin_hzzh@126.com>
2023-03-24 10:58:23 +0800

    增加弹出新页面也在自己的窗口打开

--- WinFormIISExpressHost/Form1.cs
+++ WinFormIISExpressHost/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
 using CefSharp.WinForms;
 
 namespace WinFormIISExpressHost
@@ -37,6 +38,7 @@ namespace WinFormIISExpressHost
 
             }
             chromeBrowser = new ChromiumWebBrowser("http://localhost:5180/");
+            chromeBrowser.LifeSpanHandler = new OpenPageSelf();
             panelParent.Controls.Add(chromeBrowser);
         }
         public static bool Start(string programPath, string para)
@@ -86,6 +88,35 @@ namespace WinFormIISExpressHost
                
             }
             
+        }
+        /// <summary>
+        /// 在自己窗口打开链接
+        /// </summary>
+        
+    }
+    internal class OpenPageSelf : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser)
+        {
+
+        }
+
+        public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser)
+        {
+
+        }
+
+        public bool OnBeforePopup(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            var chromiumWebBrowser = (ChromiumWebBrowser)browserControl;
+            chromiumWebBrowser.Load(targetUrl);
+            return true; //Return true to cancel the popup creation copyright by codebye.com.
         }
     }
 }
